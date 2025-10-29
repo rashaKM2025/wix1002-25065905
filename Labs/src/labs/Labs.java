c5d5b249fa750ee008b05bdc46a1a6d17680ab4a
acer <acer@LAPTOP-B2PO4R7J>
2025-10-29 22:21:19 +0800

    push

--- Labs/src/labs/Labs.java
+++ Labs/src/labs/Labs.java
@@ -14,14 +14,7 @@ public class Labs {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Faculty of Computer Science and Information Technology");
-        System.out.print("Faculty ");
-        System.out.print("of ");
-        System.out.print("Computer ");
-        System.out.print("Science ");
-        System.out.print("and ");
-        System.out.print("Information ");
-        System.out.print("Technology ");
+       
         // TODO code application logic here
     }
     
