7301ab5348d9f011619952ac317ceac96ce0061b
acer <acer@LAPTOP-B2PO4R7J>
2025-10-29 23:48:45 +0800

    push

--- Tutorials/src/Tutorial2/T2Q1/src/t2q1/T2Q1.java
+++ Tutorials/src/Tutorial2/T2Q1/src/t2q1/T2Q1.java
@@ -14,6 +14,14 @@ public class T2Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Faculty of Computer Science and Information Technology");
+        System.out.print("Faculty ");
+        System.out.print("of ");
+        System.out.print("Computer ");
+        System.out.print("Science ");
+        System.out.print("and ");
+        System.out.print("Information ");
+        System.out.print("Technology ");
         // TODO code application logic here
     }
     
