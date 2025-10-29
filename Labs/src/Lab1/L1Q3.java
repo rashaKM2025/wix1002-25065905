8fafbcc1c565e99f39db89e43ff8c140bc454497
acer <acer@LAPTOP-B2PO4R7J>
2025-10-29 22:55:09 +0800

    push

---
+++ Labs/src/Lab1/L1Q3.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab1;
+
+/**
+ *
+ * @author acer
+ */
+public class L1Q3 {
+     public static void main(String[] args) {
+        System.out.println("*********      ***       *       *");
+        System.out.println("*       *    *     *    ***     * *");
+        System.out.println("*       *   *       *  *****   *   *");
+        System.out.println("*       *   *       *    *    *     *");
+        System.out.println("*       *   *       *    *   *       *");
+        System.out.println("*       *   *       *    *    *     *");
+        System.out.println("*       *   *       *    *     *   *");
+        System.out.println("*       *    *     *     *      * *");
+        System.out.println("*********      ***       *       *");
+     }
+}
---
+++ Labs/src/Lab1/MyProfile.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab1;
+
+/**
+ *
+ * @author acer
+ */
+public class MyProfile {
+    
+    public static void main(String[] args) {
+    System.out.println("FULL NAME: RASHA KULTSUM MAHDIYAH");
+    System.out.println("MATRIC NO: 25065905");
+    System.out.println("DEPARTMENT: COMPUTER SCIENCE (ARTIFICIAL INTELLIGENCE)");
+    System.out.println("ADDRESS: KK13 @ UM");
+    System.out.println("EMAIL: 25065905@siswa.um.edu.my");
+    System.out.println("TELEPHONE NUMBER: 01175010031");
+    }
+}
