95506722ca145977e347ddccf2f4e9b51f4947de
acer <acer@LAPTOP-B2PO4R7J>
2025-10-29 22:03:24 +0800

    push

---
+++ Labs/src/labs/Labs.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labs;
+
+/**
+ *
+ * @author acer
+ */
+public class Labs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Faculty of Computer Science and Information Technology");
+        System.out.print("Faculty ");
+        System.out.print("of ");
+        System.out.print("Computer ");
+        System.out.print("Science ");
+        System.out.print("and ");
+        System.out.print("Information ");
+        System.out.print("Technology ");
+        // TODO code application logic here
+    }
+    
+}
