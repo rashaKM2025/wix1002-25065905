665196089faca1e7dc3428d343223036250f5906
acer <acer@LAPTOP-B2PO4R7J>
2025-11-01 14:59:37 +0800

    push

--- Labs/Lab2/L2Q1/src/l2q1/L2Q1.java
+++ Labs/Lab2/L2Q1/src/l2q1/L2Q1.java
@@ -8,12 +8,19 @@ package l2q1;
  *
  * @author acer
  */
+import java.util.Scanner;
 public class L2Q1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Fahrenheit degree: ");
+        double Fahrenheit = input.nextDouble();
+        double Celsius = (Fahrenheit - 32) / 1.8 ;
+        System.out.printf("Celsius = %.2f\n", Celsius);
+        
         // TODO code application logic here
     }
     
