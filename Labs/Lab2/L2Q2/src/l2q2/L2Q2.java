c9062f6292b777b1c8eaba271938fb86044afd7a
acer <acer@LAPTOP-B2PO4R7J>
2025-11-01 15:41:03 +0800

    push

--- Labs/Lab2/L2Q2/src/l2q2/L2Q2.java
+++ Labs/Lab2/L2Q2/src/l2q2/L2Q2.java
@@ -8,12 +8,24 @@ package l2q2;
  *
  * @author acer
  */
+import java.util.Scanner;
 public class L2Q2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("The price of the car: ");
+        double P = input.nextDouble();
+        System.out.print("Down Payment: ");
+        double D = input.nextDouble();
+        System.out.print("Interest rate in %: ");
+        double R = input.nextDouble();
+        System.out.print("Loan duration in year: ");
+        int Y = input.nextInt();
+        double M =  (P - D) * (1 + R*Y/100) / (Y *12);
+        System.out.printf("Monthly payment = %.2f\n", M);
         // TODO code application logic here
     }
     
